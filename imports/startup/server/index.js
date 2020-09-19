00f75300bde26bf6d1305d77a4482ee0f27ec899
Bruno Naschpitz <naschpitz@gmail.com>
2020-09-19 00:30:30 -0300

    Collections index creation implemented.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -30,3 +30,29 @@ import '../../api/users/server/methods.js';
 
 import '../../api/videos/server/methods.js';
 import '../../api/videos/server/publications.js';
+
+//====================================================================================================================//
+
+import CamerasCol from '../../api/cameras/both/collection.js';
+import FramesCol from '../../api/frames/both/collection.js';
+import MaterialsCol from '../../api/materials/both/collection.js';
+import NonSolidObjectsCol from '../../api/nonSolidObjects/both/collection.js';
+import ObjectsPropertiesCol from '../../api/objectsProperties/both/collection.js';
+import SceneriesCol from '../../api/sceneries/both/collection.js';
+import ServersCol from '../../api/servers/both/collection.js';
+import SimulationsCol from '../../api/simulations/both/collection.js';
+import SimulationsLogsCol from '../../api/simulationsLogs/both/collection.js';
+import SolidObjectsCol from '../../api/solidObjects/both/collection.js';
+import VideosCol from '../../api/videos/both/collection.js';
+
+CamerasCol.rawCollection().createIndex({owner: 1}, {background: true});
+FramesCol.rawCollection().createIndex({owner: 1}, {background: true});
+MaterialsCol.rawCollection().createIndex({owner: 1}, {background: true});
+NonSolidObjectsCol.rawCollection().createIndex({owner: 1}, {background: true});
+ObjectsPropertiesCol.rawCollection().createIndex({owner: 1}, {background: true});
+SceneriesCol.rawCollection().createIndex({owner: 1}, {background: true});
+ServersCol.rawCollection().createIndex({owner: 1}, {background: true});
+SimulationsCol.rawCollection().createIndex({owner: 1}, {background: true});
+SimulationsLogsCol.rawCollection().createIndex({owner: 1}, {background: true});
+SolidObjectsCol.rawCollection().createIndex({owner: 1}, {background: true});
+VideosCol.collection.rawCollection().createIndex({'meta.owner': 1}, {background: true});
