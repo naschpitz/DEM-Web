f663a468cd7d5481f9609d855b6a5f7a2113bfd7
Bruno Naschpitz <naschpitz@gmail.com>
2022-09-04 11:44:44 -0300

    <DataSet/> delete button implemented.

--- imports/ui/components/simulations/simulation/calibration/dataSets/dataSet/dataSet.jsx
+++ imports/ui/components/simulations/simulation/calibration/dataSets/dataSet/dataSet.jsx
@@ -6,6 +6,8 @@ import CalibrationsClass from "../../../../../../../api/calibrations/both/class.
 import SceneriesClass from "../../../../../../../api/sceneries/both/class.js"
 
 import Alert from "react-s-alert"
+import { ButtonEnhanced } from "@naschpitz/button-enhanced"
+import { FaTrashAlt } from "react-icons/fa"
 
 import Chart from "./chart/chart.jsx"
 import DataImporter from "./dataImporter/dataImporter.jsx"
@@ -16,6 +18,7 @@ import "./dataSet.css"
 export default DataSet = props => {
   const [isCalibrationReady, setIsCalibrationReady] = useState(false)
   const [isSceneryReady, setIsSceneryReady] = useState(false)
+  const [isRemovingDataSet, setIsRemovingDataSet] = useState(false)
 
   useTracker(() => {
     if (!props.dataSet) return
@@ -61,12 +64,29 @@ export default DataSet = props => {
       if (error) {
         Alert.error("Error: " + getErrorMessage(error))
       } else {
-        Alert.success("DataSet successfully updated.")
+        Alert.success("Data set successfully updated.")
       }
     })
   }
 
+  function onRemoveDataSetDone(result, data) {
+    if (!result) return
+
+    setIsRemovingDataSet(true)
+
+    Meteor.call("dataSets.remove", data, error => {
+      if (error) {
+        Alert.error("Error: " + getErrorMessage(error))
+      } else {
+        Alert.success("Data set successfully removed.")
+      }
+
+      setIsRemovingDataSet(false)
+    })
+  }
+
   const sceneryId = scenery?._id
+  const dataSetId = props.dataSet?._id
 
   const objectId = props.dataSet?.object
   const dataName = props.dataSet?.dataName
@@ -76,7 +96,27 @@ export default DataSet = props => {
     <div id="dataSet">
       <div className="card">
         <div className="card-header">
-          <div className="panel-title">Data Set</div>
+          <div className="panel-title">
+            Data Set &nbsp;
+            <ButtonEnhanced
+              buttonOptions={{
+                regularText: <FaTrashAlt className="align-middle" />,
+                className: "btn btn-sm btn-danger",
+                data: dataSetId,
+                isAction: isRemovingDataSet,
+                actionText: <div className="loaderSpinner loader-small" />,
+                type: "button",
+              }}
+              confirmationOptions={{
+                title: "Confirm data set removal",
+                text: <span>Do you really want to remove this data set?</span>,
+                confirmButtonText: "Remove",
+                confirmButtonAction: "Removing...",
+                cancelButtonText: "Cancel",
+                onDone: onRemoveDataSetDone,
+              }}
+            />
+          </div>
         </div>
 
         <div className="card-body">
