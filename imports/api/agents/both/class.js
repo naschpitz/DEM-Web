a9af5d498ee2625d432b20310f320f27c5f24442
Bruno Naschpitz <naschpitz@gmail.com>
2022-12-10 17:29:56 -0300

    Agent.calculateScore() implemented.

--- imports/api/agents/both/class.js
+++ imports/api/agents/both/class.js
@@ -1,14 +1,19 @@
 import AgentsDAO from "./dao"
 
 import Calibrations from "../../calibrations/both/class"
-import Simulations from "../../simulations/both/class"
+import DataSets from "../../dataSets/both/class"
+import Frames from "../../frames/both/class"
 import Materials from "../../materials/both/class"
+import NonSolidObjects from "../../nonSolidObjects/both/class"
 import Sceneries from "../../sceneries/both/class"
+import Simulations from "../../simulations/both/class"
+import SolidObjects from "../../solidObjects/both/class"
+
+import Spline from "cubic-spline"
 
 export default class Agents extends AgentsDAO {
   static create(calibrationId, index) {
     const calibration = Calibrations.findOne(calibrationId)
-
     const simulation = Simulations.findOne(calibration.owner)
 
     // Clones the original simulation (thus, scenery and materials). The cloned simulation is not primary, as it is
@@ -75,5 +80,52 @@ export default class Agents extends AgentsDAO {
     const simulation = Simulations.findOne(agent.simulation)
 
     return simulation.state
+  }
+
+  static calculateScore(agentId) {
+    const agent = Agents.findOne(agentId)
+    const simulation = Simulations.findOne(agent.simulation)
+    const scenery = Sceneries.findOne({ owner: simulation._id })
+
+    const frames = Frames.find({ owner: scenery._id }, { sort: { time: 1 } }).fetch()
+
+    let totalScore = 0
+
+    DataSets.find({ owner: agent.owner }).forEach(dataSet => {
+      const objectId = dataSet.object
+      const object = NonSolidObjects.findOne(objectId) || SolidObjects.findOne(objectId)
+
+      const objectCallSign = object.callSign
+      const dataName = dataSet.dataName
+
+      const spline = new Spline(
+        dataSet.data.map(data => data.time),
+        dataSet.data.map(data => data.value)
+      )
+
+      totalScore += frames.reduce((score, frame) => {
+        // Get the non-solid or solid object that belongs to the Frame's Scenery and has the same callSign as the DataSet's
+        const nonSolidObject = NonSolidObjects.findOne({ owner: frame.owner, callSign: objectCallSign })
+        const solidObject = SolidObjects.findOne({ owner: frame.owner, callSign: objectCallSign })
+
+        const object = nonSolidObject || solidObject
+
+        // Find in the frame object's list the one that matches the id of the object found above
+        const frameNonSolidObject = _.find(frame.object.nonSolidObjects, { _id: object._id })
+        const frameSolidObject = _.find(frame.object.solidObjects, { _id: object._id })
+
+        const frameObject = frameNonSolidObject || frameSolidObject
+
+        // Get the value of the dataName in the frame object
+        const value = frameObject[dataName]
+
+        // Calculate the difference between the value and the expected value
+        const error = Math.abs(value - spline.at(frame.time))
+
+        return score + error
+      }, 0)
+    })
+
+    return totalScore
   }
 }
