75f1b17d0ac18070199f5f711a10568b5cb34db7
Bruno Naschpitz <naschpitz@gmail.com>
2023-02-17 15:27:49 -0300

    Added to the Simulations job the case to check for Simulations that are currently in the 'running' state but didn't get a log update since 10 times the logTime ago.

--- imports/api/simulations/server/jobs.js
+++ imports/api/simulations/server/jobs.js
@@ -2,20 +2,35 @@ import { Meteor } from "meteor/meteor"
 import { CronJob } from "cron"
 import moment from "moment"
 
+import Logs from "../../logs/both/class"
 import Simulations from "../both/class"
 
-// Find stalled Simulations and set its state to 'fail'
+// Find stalled Simulations and set its state to 'failed'
 const bound = Meteor.bindEnvironment(() => {
   const tenSecondsAgo = moment().subtract(10, "seconds").toDate()
 
   const stalledSimulations = Simulations.find({
     state: { $in: ["setToRun", "setToPause", "setToStop"] },
     updatedAt: { $lte: tenSecondsAgo },
+  }).fetch()
+
+  // For every simulation in the state "running" or "paused", check if the latest log message is older than 10 times the
+  // logTime. If it is, add the simulation to the list of stalled simulations.
+  Simulations.find({ state: { $in: ["running"] } }).forEach(simulation => {
+    const latestLog = Logs.findOne({ owner: simulation._id }, { sort: { createdAt: -1 } })
+
+    const tenTimesLogTimeAgo = moment()
+      .subtract(10 * simulation.logTime, "seconds")
+      .toDate()
+
+    if (latestLog.createdAt <= tenTimesLogTimeAgo) {
+      stalledSimulations.push(simulation)
+    }
   })
 
   // Will not continue with the job if there are no stalled simulations.
   // Avoids console.log pollution.
-  if (stalledSimulations.count() === 0) {
+  if (stalledSimulations.length === 0) {
     return
   }
 
