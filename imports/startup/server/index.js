8acb8017279d9f343a34341f78c12a0e70de182f
Bruno Naschpitz <naschpitz@gmail.com>
2025-06-25 21:28:49 -0300

    Imports reordered.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -4,16 +4,18 @@ import "../../api/agents/server/publications.js"
 
 import "../../api/agentsHistories/server/publications.js"
 
-import "../../api/calibrations/server/task/restartCalibrations.js"
-
 import "../../api/calibrations/server/methods.js"
 import "../../api/calibrations/server/publications.js"
-
-import "../../api/cameras/server/publications.js"
+import "../../api/calibrations/server/task/restartCalibrations.js"
 
 import "../../api/cameraFilters/server/publications.js"
 
+import "../../api/cameras/server/publications.js"
+
 import "../../api/dataSets/server/publications.js"
+
+import "../../api/files/server/fileServer.js"
+import "../../api/files/server/publications.js"
 
 import "../../api/frames/server/methods.js"
 import "../../api/frames/server/publications.js"
@@ -22,8 +24,6 @@ import "../../api/framesImages/server/methods.js"
 
 import "../../api/groups/server/methods.js"
 import "../../api/groups/server/publications.js"
-
-import "../../api/scheduler/server/scheduler.js"
 
 import "../../api/logs/server/publications.js"
 
@@ -40,11 +40,13 @@ import "../../api/rest/server/rest.js"
 import "../../api/sceneries/server/methods.js"
 import "../../api/sceneries/server/publications.js"
 
+import "../../api/scheduler/server/scheduler.js"
+
 import "../../api/servers/server/publications.js"
 
-import "../../api/simulations/server/tasks/checkStalledSimulations.js"
 import "../../api/simulations/server/methods.js"
 import "../../api/simulations/server/publications.js"
+import "../../api/simulations/server/tasks/checkStalledSimulations.js"
 
 import "../../api/solidObjects/server/publications.js"
 
@@ -52,5 +54,3 @@ import "../../api/users/server/methods.js"
 
 import "../../api/videos/server/methods.js"
 import "../../api/videos/server/publications.js"
-import "../../api/files/server/publications.js"
-import "../../api/files/server/fileServer.js"
